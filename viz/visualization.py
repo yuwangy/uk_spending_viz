65ecc10883cee19bc4478f63be33eb33a58b78b1
Yuyang Wang <franktcsn@gmail.com>
2023-09-30 23:51:54 -0700

    add files

--- viz/visualization.py
+++ viz/visualization.py
@@ -3,6 +3,15 @@
 import pandas as pd
 import altair as alt
 
-url_path = 'https://raw.githubusercontent.com/kemiolamudzengi/dsci-320-datasets/main/tinder.csv'
+url_path = 'https://raw.githubusercontent.com/yuwangy/uk_spending_viz/main/Government%20spending%202010-11%20-%20TOTALS.csv'
 data = pd.read_csv(url_path)
-data.head(10)
+data.head(8)
+
+data.shape
+data.info()
+
+# data filtering and cleaning
+data = data.rename(columns = {'Unnamed: 1': '09_10_spending',
+'Unnamed: 2': '10_11_spending'})
+data.head(5)
+
