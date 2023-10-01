8952320e1901ae1720b86d080a08736735e92ac6
Yuyang Wang <franktcsn@gmail.com>
2023-09-30 23:33:37 -0700

    add viz

--- viz/visualization.py
+++ viz/visualization.py
@@ -1,2 +1,8 @@
 # visualization redesign
 
+import pandas as pd
+import altair as alt
+
+url_path = 'https://raw.githubusercontent.com/kemiolamudzengi/dsci-320-datasets/main/tinder.csv'
+data = pd.read_csv(url_path)
+data.head(10)
